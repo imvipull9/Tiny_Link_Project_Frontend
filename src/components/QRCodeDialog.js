6e4376fe15c1665dea161e6733c1434ac2b7a198
Vipul Saini <122309708+imvipull9@users.noreply.github.com>
2025-11-23 22:41:50 +0530

    Update QRCodeDialog.js

--- src/components/QRCodeDialog.js
+++ src/components/QRCodeDialog.js
@@ -1,17 +1,56 @@
 import React from "react";
-import { Dialog, DialogTitle, DialogContent } from "@mui/material";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  Box,
+  Typography,
+} from "@mui/material";
 import { QRCodeCanvas } from "qrcode.react";
 
 function QRCodeDialog({ open, onClose, url }) {
-  if (!url) return null;
+  if (!open || !url) return null;
 
   return (
-    <Dialog open={open} onClose={onClose}>
-      <DialogTitle>QR Code</DialogTitle>
-      <DialogContent sx={{ textAlign: "center" }}>
-        <QRCodeCanvas value={url} size={200} />
-        <p style={{ wordBreak: "break-all", marginTop: 8 }}>{url}</p>
+    <Dialog
+      open={open}
+      onClose={onClose}
+      maxWidth="xs"
+      fullWidth
+    >
+      <DialogTitle sx={{ fontWeight: 700, textAlign: "center" }}>
+        QR Code
+      </DialogTitle>
+
+      <DialogContent sx={{ textAlign: "center", py: 3 }}>
+        {/* QR Code */}
+        <Box sx={{ mb: 2 }}>
+          <QRCodeCanvas value={url} size={200} />
+        </Box>
+
+        {/* URL Display */}
+        <Typography
+          variant="body2"
+          sx={{
+            wordBreak: "break-all",
+            backgroundColor: "background.paper",
+            p: 1,
+            borderRadius: 1,
+            border: "1px solid",
+            borderColor: "divider",
+          }}
+        >
+          {url}
+        </Typography>
       </DialogContent>
+
+      <DialogActions sx={{ justifyContent: "center", pb: 2 }}>
+        <Button variant="contained" onClick={onClose}>
+          Close
+        </Button>
+      </DialogActions>
     </Dialog>
   );
 }
